21e990a29fbd40224d3637832e13b7d87b19c3d1
Marco Radavelli <marco.radavelli@unibg.it>
2018-12-07 12:10:41 +0100

    update matrice

---
+++ EsempiIJAC1819/src/ExAnd.java
@@ -0,0 +1,9 @@
+
+public class ExAnd {
+	public static void main(String[] args) {
+		int a=5, b=10;
+		for (int i=0; i<a || i<b; i++) {
+			System.out.println(i);
+		}
+	}
+}
--- EsempiIJAC1819/src/Matrice.java
+++ EsempiIJAC1819/src/Matrice.java
@@ -10,17 +10,23 @@ public class Matrice {
 	}
 
 	public void stampaMatrice() {
-		System.out.println(this);
+		System.out.println(this );
 	}
 	
 	@Override
 	public String toString() {
-		int i, j;
+		//int i, j;
 		StringBuffer sb = new StringBuffer();
-		for (i = 0; i < numeroRighe; i++) {
+		/*for (i = 0; i < numeroRighe; i++) {
 			for (j = 0; j < numeroColonne; j++)
 				sb.append(matrix[i][j] + " ");
 			sb.append("\n");
+		}*/
+		for (int i=0; i<numeroRighe*numeroColonne; i++) {
+			sb.append(matrix[i/numeroColonne][i%numeroColonne]);
+			if (i==numeroColonne-1) {
+				sb.append("\n");
+			}
 		}
 		return sb.toString();
 	}
